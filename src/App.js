d8d01878921d524b038c854ed2300bbc7f931d8d
YashwanthNagaraju <yashwanthn241@gmail.com>
2022-11-19 22:00:47 +1100

    added app.js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import { createContext, useState } from "react";
+import { pageRoutes } from "./components/pages/handlers/pageRoutes";
+
+export const ThemeContext = createContext();
+
+function App() {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <Router>
+      <ThemeContext.Provider value={{ isOpen, setOpen }}>
+        <AppContainer className="App">
+          <Switch>
+            {pageRoutes.map((page) => (
+              <Route
+                exact
+                key={page.key}
+                path={page.pathname}
+                component={page.component}
+              />
+            ))}
+          </Switch>
+        </AppContainer>
+      </ThemeContext.Provider>
+    </Router>
+  );
+}
+
+export default App;
+
+const AppContainer = styled.div``;
