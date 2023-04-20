104b16fd44f070b572c4524dcb4545a3bccdac04
Yashwanth Nagaraju <43926836+YashwanthNagaraju@users.noreply.github.com>
2023-04-20 22:07:50 +1000

    Delete email.js

--- src/components/pages/handlers/email.js
+++
@@ -1,9 +0,0 @@
-import nodemailer from "nodemailer";
-
-export const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'yashwanthn241@gmail.com',
-    pass: 'Dnivra,2411998'
-  }
-});
